dd2e0ed35f1cdd9b437a54bef55b569a4812be28
root <root@devuan>
2020-03-16 11:56:25 +0200

    add basic 4g band selection

--- huaweisms/api/device.py
+++ huaweisms/api/device.py
@@ -30,3 +30,44 @@ def reboot(ctx):
 
     payload = '<?xml version: "1.0" encoding="UTF-8"?><request><Control>1</Control></request>'
     return post_to_url(url, payload, ctx, additional_headers=headers)
+
+def net1800(ctx):
+    # type: (ApiCtx) -> dict
+    """
+    Reboots the modem.
+    """
+
+    url = '{}/net/net-mode'.format(ctx.api_base_url)
+    headers = {
+        '__RequestVerificationToken': ctx.token,
+    }
+
+    payload = '<?xml version: "1.0" encoding="UTF-8"?><request><NetworkMode>03</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>4</LTEBand></request>'
+    return post_to_url(url, payload, ctx, additional_headers=headers)
+
+def net2600(ctx):
+    # type: (ApiCtx) -> dict
+    """
+    Reboots the modem.
+    """
+
+    url = '{}/net/net-mode'.format(ctx.api_base_url)
+    headers = {
+        '__RequestVerificationToken': ctx.token,
+    }
+
+    payload = '<?xml version: "1.0" encoding="UTF-8"?><request><NetworkMode>03</NetworkMode><NetworkBand>3FFFFFFF</NetworkBand><LTEBand>2000000000</LTEBand></request>'
+    return post_to_url(url, payload, ctx, additional_headers=headers)
+
+
+def netall(ctx):
+    # type: (ApiCtx) -> dict
+    """
+    Reboots the modem.
+    """
+
+    url = '{}/net/net-mode'.format(ctx.api_base_url)
+    headers = {
+        '__RequestVerificationToken': ctx.token,
+    }
+
